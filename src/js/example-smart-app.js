ff77c2cb32d55cd0f390a2660dcaab88c9deed42
owaismazik <63541338+owaismazik@users.noreply.github.com>
2020-09-18 20:49:01 +0500

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -200,7 +200,7 @@
                                             }
                                             item.type = 8;
                                             item.id = dataSet.conditionID;
-                                            if (ondition[i].hasOwnProperty(encounter)) {
+                                            if (condition[i].hasOwnProperty(encounter)) {
                                                 item.encounterID = condition[i].encounter.reference.split('/')[1];
                                             }
                                             item.entity = "Condition";

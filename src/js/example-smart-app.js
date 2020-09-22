7a5252784e30747bf47f36eda4d78a3e37133404
owaismazik <63541338+owaismazik@users.noreply.github.com>
2020-09-22 14:02:50 +0500

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -914,17 +914,36 @@
                                         var daydivmonth = $("#" + year).find(".timeline__box").find("." + month).length;
 
                                         if (daydivcount > 0 && daydivmonth > 0) {
-                                            html = '<div class="timeline__box  mzkheight mzktimelinebox">' +
-                                                '<div class="timeline__post">' +
-                                                '<div class="timeline__content"> ' +
-                                                '<span id="' + id + '_' +year+ '" encounterID="' + encounterID + '" class="timelineentity">' + entity + ' ' + id+ '</span>' +
-                                                '<p> ' + name + '</p>' +
-                                                '<span class="mzkicon ' + spanClass+'">' +
-                                                '<img class="mzkimg ' + imgClass+'" src="https://owaismazik.github.io/PatientTimeLine/src/images/' + imageName +'">' +
-                                                '</span>' +
-                                                '</div></div></div>';
+                                            if (encounterID != 'undefined') {
+                                                html = '<div class="timeline__box  mzkheight mzktimelinebox">' +
+                                                    '<div class="timeline__post">' +
+                                                    '<div class="timeline__content"> ' +
+                                                    '<span id="' + id + '_' + year + '" encounterID="' + encounterID + '" class="timelineentity">' + entity + ' ' + id + '</span>' +
+                                                    '<p> ' + name + '</p>' +
+                                                    '<span class="mzkicon ' + spanClass + '">' +
+                                                    '<img class="mzkimg ' + imgClass + '" src="https://owaismazik.github.io/PatientTimeLine/src/images/' + imageName + '">' +
+                                                    '</span>' +
+                                                    '</div></div></div>';
+                                            }
                                         }
                                         else {
+                                            if (encounterID != 'undefined') {
+                                                html = '<div class="timeline__box  mzkheight mzktimelinebox"><div class="timeline__date">' +
+                                                    '<span class="timeline__day ' + day + '">' + day + '</span>' +
+                                                    '<span class="timeline__month ' + month + '">' + month + '</span></div>' +
+                                                    '<div class="timeline__post">' +
+                                                    '<div class="timeline__content"> ' +
+                                                    '<span id="' + id + '_' + year + '" encounterID="' + encounterID + '" class="timelineentity">' + entity + ' ' + id + '</span>' +
+                                                    '<p> ' + name + '</p>' +
+                                                    '<span class="mzkicon ' + spanClass + '">' +
+                                                    '<img class="mzkimg ' + imgClass + '" src="https://owaismazik.github.io/PatientTimeLine/src/images/' + imageName + '">' +
+                                                    '</span>' +
+                                                    '</div></div></div>';
+                                            }
+                                        }
+                                    }
+                                    else {
+                                        if (encounterID != 'undefined') {
                                             html = '<div class="timeline__box  mzkheight mzktimelinebox"><div class="timeline__date">' +
                                                 '<span class="timeline__day ' + day + '">' + day + '</span>' +
                                                 '<span class="timeline__month ' + month + '">' + month + '</span></div>' +
@@ -938,7 +957,9 @@
                                                 '</div></div></div>';
                                         }
                                     }
-                                    else {
+                                }
+                                else {
+                                    if (encounterID != 'undefined') {
                                         html = '<div class="timeline__box  mzkheight mzktimelinebox"><div class="timeline__date">' +
                                             '<span class="timeline__day ' + day + '">' + day + '</span>' +
                                             '<span class="timeline__month ' + month + '">' + month + '</span></div>' +
@@ -951,19 +972,6 @@
                                             '</span>' +
                                             '</div></div></div>';
                                     }
-                                }
-                                else {
-                                    html = '<div class="timeline__box  mzkheight mzktimelinebox"><div class="timeline__date">' +
-                                        '<span class="timeline__day ' + day + '">' + day + '</span>' +
-                                        '<span class="timeline__month ' + month + '">' + month + '</span></div>' +
-                                        '<div class="timeline__post">' +
-                                        '<div class="timeline__content"> ' +
-                                        '<span id="' + id + '_' + year + '" encounterID="' + encounterID + '" class="timelineentity">' + entity + ' ' + id + '</span>' +
-                                        '<p> ' + name + '</p>' +
-                                        '<span class="mzkicon ' + spanClass + '">' +
-                                        '<img class="mzkimg ' + imgClass + '" src="https://owaismazik.github.io/PatientTimeLine/src/images/' + imageName + '">' +
-                                        '</span>' +
-                                        '</div></div></div>';
                                 }
                             }
                             $("#" + year).append(html);

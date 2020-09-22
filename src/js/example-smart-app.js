f6dadfffe8f58e5abb28318bd7745324420fbde7
owaismazik <63541338+owaismazik@users.noreply.github.com>
2020-09-22 20:11:51 +0500

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -13,16 +13,6 @@
             if (smart.hasOwnProperty('patient')) {
                 var patient = smart.patient;
                 var pt = patient.read();
-                var obv = smart.patient.api.fetchAll({
-                    type: 'Observation',
-                    query: {
-                        code: {
-                            $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                                'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                                'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                        }
-                    }
-                });
 
                 $.when(pt, obv).fail(onError);
 
@@ -118,6 +108,17 @@
                                         }
                                     }
                                 }
+                            }
+                        }
+                    });
+
+                    var obv = smart.patient.api.fetchAll({
+                        type: 'Observation',
+                        query: {
+                            code: {
+                                $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                                    'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                                    'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
                             }
                         }
                     });

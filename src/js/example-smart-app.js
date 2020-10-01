190729999b8a239558304313da47eb06933f70de
owaismazik <63541338+owaismazik@users.noreply.github.com>
2020-10-01 16:51:38 +0500

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -943,7 +943,7 @@
                                                         '<img class="mzkimg ' + imgClass + '" src="https://owaismazik.github.io/PatientTimeLine/src/images/' + imageName + '">' +
                                                         '</span>' +
                                                         '</div></div>';
-                                                    $(idEncounter).parent().parent().parent().parent().append(html);
+                                                    $(idEncounter).parent().parent().parent().siblings().append(html);
                                                     html = "";
                                                 }
                                                 else {
@@ -1243,7 +1243,7 @@
                                                         '<img class="mzkimg ' + imgClass + '" src="https://owaismazik.github.io/PatientTimeLine/src/images/' + imageName + '">' +
                                                         '</span>' +
                                                         '</div></div>';
-                                                    $(idEncounter).parent().parent().parent().parent().append(html);
+                                                    $(idEncounter).parent().parent().parent().siblings().append(html);
                                                     html = "";
                                                 }
                                                 else {

267d59ec29177c02a7348b3548dcef62d39c100b
owaismazik <63541338+owaismazik@users.noreply.github.com>
2020-10-01 22:29:48 +0500

    Update example-smart-app.js

--- src/js/example-smart-app.js
+++ src/js/example-smart-app.js
@@ -990,9 +990,10 @@
                                                     html = "";
                                                 }
                                                 else {
-                                                    html = '<div class="timeline__box mzkheight mzktimelinebox"><div class="timeline__date mzkpanalchild">' +
-                                                        '<span class="timeline__day ' + day + '">' + day + '</span>' +
-                                                        '<span class="timeline__month ' + month + '">' + month + '</span></div>' +
+                                                    html = '<div class="timeline__box mzkheight mzktimelinebox">' +
+                                                        // TODO testing
+                                                        //'<span class="timeline__day ' + day + '">' + day + '</span>' +
+                                                        //'<span class="timeline__month ' + month + '">' + month + '</span></div>' +
                                                         '<div class="timeline__post">' +
                                                         '<div class="timeline__content"> ' +
                                                         '<span id="' + id + '" encounterID="' + encounterID + '" class="timelineentity">' + entity + '</span>' +
